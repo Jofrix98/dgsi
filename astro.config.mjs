1aedcce9d9e0314a38aa78d4d1ee999bf0a62c57
Jofrix98 <jofre.coll10@gmail.com>
2025-05-29 20:11:07 +0200

    Remove tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Jofrix98.github.io',
   base: '/dgsi',
-  integrations: [tailwind()],
 });
